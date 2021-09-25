8a38a15c01809974a1902d8a1a05c689df289685
Cookiekira <1308132372@qq.com>
2021-09-25 10:36:52 +0800

    Add LocalStorage

--- src/composables/useTodos.ts
+++ src/composables/useTodos.ts
@@ -2,21 +2,27 @@ import { ref } from "vue";
 
 export default function useTodos() {
     const todos = ref<any[]>([]);
+    todos.value = JSON.parse(localStorage.todos);
 
     // 添加 todo
     const addTodo = (todo: { content: string }) => {
         todo.content !== ""
             ? todos.value.push(todo)
             : console.log("输入不能为空");
+        localStorage.todos = JSON.stringify(todos.value);
     };
     //根据时间戳删除
-    const deleteTodo = (id:number) => {
-        todos.value.splice(todos.value.findIndex(todo => todo.id === id), 1)
+    const deleteTodo = (id: number) => {
+        todos.value.splice(
+            todos.value.findIndex((todo) => todo.id === id),
+            1
+        );
+        localStorage.todos = JSON.stringify(todos.value);
     };
 
     return {
         todos,
         addTodo,
-        deleteTodo
+        deleteTodo,
     };
 }
