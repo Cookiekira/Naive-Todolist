1465cb92881e8dcd3f27706a9a0fe52306243d55
Cookiekira <1308132372@qq.com>
2021-09-25 11:07:24 +0800

    Fixed LocalStorage

--- src/composables/useTodos.ts
+++ src/composables/useTodos.ts
@@ -1,23 +1,31 @@
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 export default function useTodos() {
     const todos = ref<any[]>([]);
-    todos.value = JSON.parse(localStorage.todos);
+
+    //读取LocalStorage
+    if (typeof localStorage.todos !== "undefined") {
+        todos.value = JSON.parse(localStorage.todos);
+    }
+
+    //使用Watch Api 存储TodoList
+    watch(todos.value, () => {
+        localStorage.todos = JSON.stringify(todos.value);
+    });
 
     // 添加 todo
     const addTodo = (todo: { content: string }) => {
         todo.content !== ""
             ? todos.value.push(todo)
             : console.log("输入不能为空");
-        localStorage.todos = JSON.stringify(todos.value);
     };
+
     //根据时间戳删除
     const deleteTodo = (id: number) => {
         todos.value.splice(
             todos.value.findIndex((todo) => todo.id === id),
             1
         );
-        localStorage.todos = JSON.stringify(todos.value);
     };
 
     return {
